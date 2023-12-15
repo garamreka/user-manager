0183f0e6a182e7bfa36750dbcafed76237912e4e
garamreka <garam.reka@gmail.com>
2023-12-15 13:48:06 +0100

    feat(): set up MongoDb connection

--- UserManager.Api/Program.cs
+++ UserManager.Api/Program.cs
@@ -1,3 +1,6 @@
+using MongoDB.Driver;
+using UserManager.Api.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+var mongoClient = new MongoClient(configuration.GetConnectionString("MongoDb"));
+builder.Services.AddSingleton<IMongoClient>(mongoClient);
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
