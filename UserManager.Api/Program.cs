b003dfabb34d971de36bde7248b900c84f9018ec
garamreka <garam.reka@gmail.com>
2023-12-15 15:16:29 +0100

    feat(Program.cs): ensure lowercase urls

--- UserManager.Api/Program.cs
+++ UserManager.Api/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 IConfigurationRoot configuration = new ConfigurationBuilder()
            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
