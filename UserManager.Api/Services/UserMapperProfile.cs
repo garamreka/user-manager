f9a86d22b052c862c8a1e6019ee2e973693eafc8
garamreka <garam.reka@gmail.com>
2023-12-17 00:48:19 +0100

    feat(UserMapperProfile): create profile

---
+++ UserManager.Api/Services/UserMapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using UserManager.Api.DTOs;
+using UserManager.Api.Models;
+
+namespace UserManager.Api.Services
+{
+    public class UserMapperProfile : Profile
+    {
+        public UserMapperProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
+            CreateMap<UserDto, User>();
+
+            CreateMap<User, NewUserDto>();
+            CreateMap<NewUserDto, User>();
+
+            CreateMap<Address, AddressDto>();
+            CreateMap<AddressDto, Address>();
+
+            CreateMap<Company, CompanyDto>();
+            CreateMap<CompanyDto, Company>();
+
+            CreateMap<Geo, GeoDto>();
+            CreateMap<GeoDto, Geo>();
+        }
+    }
+}
