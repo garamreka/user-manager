b44bb400a230039651d8634a676a46ce4a0f5dd0
garamreka <garam.reka@gmail.com>
2023-12-17 00:42:20 +0100

    feat(Services): add service layer

---
+++ UserManager.Api/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using UserManager.Api.DTOs;
+
+namespace UserManager.Api.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> GetAllUsers();
+
+        Task<UserDto> GetUserById(string id);
+
+        Task<string> AddUser(NewUserDto user);
+
+        Task UpdateUser(string id, UserDto user);
+
+        Task DeleteUserById(string id);
+    }
+}
---
+++ UserManager.Api/Services/UserService.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using MongoDB.Bson;
+using UserManager.Api.CustomExceptions;
+using UserManager.Api.DTOs;
+using UserManager.Api.Models;
+using UserManager.Api.Repositories;
+
+namespace UserManager.Api.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper)
+        {
+            this._userRepository = userRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<string> AddUser(NewUserDto userDto)
+        {
+            var user = _mapper.Map<User>(userDto);
+            var userId = await _userRepository.AddUser(user);
+
+            return userId.ToString();
+        }
+
+        public async Task DeleteUserById(string id)
+        {
+            var success = await _userRepository.DeleteUserById(ObjectId.Parse(id));
+
+            if (!success)
+            {
+                throw new NotFoundException();
+            }
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAllUsers()
+        {
+            var users = await _userRepository.GetAllUsers();
+            return _mapper.Map<IEnumerable<UserDto>>(users);
+        }
+
+        public async Task<UserDto> GetUserById(string id)
+        {
+            var user = await _userRepository.GetUserById(ObjectId.Parse(id));
+
+            if (user is null)
+            {
+                throw new NotFoundException();
+            }
+
+            return _mapper.Map<UserDto>(user);
+        }
+
+        public async Task UpdateUser(string id, UserDto user)
+        {
+            var userToUpdate = _mapper.Map<User>(user);
+            var success = await _userRepository.UpdateUser(ObjectId.Parse(id), userToUpdate);
+
+            if (!success)
+            {
+                throw new NotFoundException();
+            }
+        }
+    }
+}
