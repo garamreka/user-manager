d841e3757527e9c68f8390ca102e0173c0376316
garamreka <garam.reka@gmail.com>
2023-12-18 01:10:35 +0100

    fix(GeoDto): remove failing regex

--- UserManager.Api/DTOs/GeoDto.cs
+++ UserManager.Api/DTOs/GeoDto.cs
@@ -1,15 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace UserManager.Api.DTOs
+﻿namespace UserManager.Api.DTOs
 {
     public class GeoDto
     {
-        private const string _coordinateRegexPattern = @"^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$";
-
-        [RegularExpression(_coordinateRegexPattern)]
         public string? Lat { get; set; }
 
-        [RegularExpression(_coordinateRegexPattern)]
         public string? Lng { get; set; }
     }
 }
